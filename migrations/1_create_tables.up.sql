d454a7e08ef00d9e799fce825612387e30f62923
Ivan, Tarasov <alukart32work@gmail.com>
2023-05-12 13:41:43 +0300

    [ITER1] Add db schema.

---
+++ migrations/1_create_tables.up.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS "users" (
+    id uuid  PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE CHECK(LENGTH(username) > 0),
+    password VARCHAR NOT NULL CHECK(LENGTH(password) > 0)
+);
+
+CREATE TYPE "blob_object_typ" AS ENUM (
+  'TEXT',
+  'BIN'
+);
+
+CREATE TABLE IF NOT EXISTS "blob_objects" (
+  id      uuid PRIMARY KEY,
+  user_id uuid NOT NULL,
+	name    VARCHAR NOT NULL CHECK(LENGTH(name) > 0),
+	typ     blob_object_typ NOT NULL,
+	notes   VARCHAR,
+  uri     VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "passwords" (
+  id      uuid PRIMARY KEY,
+  user_id uuid NOT NULL,
+	name    VARCHAR NOT NULL CHECK(LENGTH(name) > 0),
+	data    VARCHAR NOT NULL CHECK(LENGTH(data) > 0),
+	notes   VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS "credit_cards" (
+  id      uuid PRIMARY KEY,
+  user_id uuid NOT NULL,
+	name    VARCHAR NOT NULL CHECK(LENGTH(name) > 0),
+	data    VARCHAR NOT NULL CHECK(LENGTH(data) > 0),
+	notes   VARCHAR
+);
+
+ALTER TABLE "blob_objects" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "passwords" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "credit_cards" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
